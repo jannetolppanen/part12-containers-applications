fc7b880379b478fe397c6426f71a025a8ad48427
Janne Tolppanen <janne.ks@gmail.com>
2024-12-08 21:56:20 +0000

    12.7 ei aloitettu mutta siihen asti on tehty säädöt. mongodb toimii kontista ja nodebäkkäri lokaalisti

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = process.env.MONGO_URL || undefined
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:3456/the_database'
 const REDIS_URL = process.env.REDIS_URL || undefined
 
 module.exports = {
