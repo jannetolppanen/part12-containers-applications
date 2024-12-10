deb0edf269f24fdb6bf90626beee547e3b63d28c
Janne Tolppanen <janne.ks@gmail.com>
2024-12-10 07:52:55 +0000

    mongo_url lisätty suoraan configiin, päivitetty getone ja put todo. 12.7 tehty

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,5 +1,8 @@
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:3456/the_database'
+//const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:3456/the_database'
 const REDIS_URL = process.env.REDIS_URL || undefined
+
+/* Oma lisäys jotta ei tarvitse antaa mongourlia käynnistyksen yhteydessä */
+const MONGO_URL = 'mongodb://the_username:the_password@localhost:3456/the_database'
 
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
